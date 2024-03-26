2809c12f0c25c2abd050d9f4fb9ea82d88ecbf7d
Mukalingam-TAS <162333986+Mukalingam-TAS@users.noreply.github.com>
2024-03-26 11:32:25 +0530

    initial commit

---
+++ App.py
@@ -0,0 +1,198 @@
+import streamlit as st
+import os
+import tempfile
+
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain_community.vectorstores import AstraDB
+from langchain.schema.runnable import RunnableMap
+from langchain.prompts import ChatPromptTemplate
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+import base64
+import json
+
+st.set_page_config(page_title="DocuQuery",page_icon="🚀",layout="wide",initial_sidebar_state="expanded")
+
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+img = get_img_as_base64("assets/back.jpg")
+img1 = get_img_as_base64("assets/menu.jpg")
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("data:image/jpg;base64,{img}");
+background-size: 250%;
+background-position: top left;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
+[data-testid="stSidebar"] > div:first-child {{
+background-image: url("data:image/png;base64,{img1}");
+background-position: center; 
+background-repeat: no-repeat;
+background-attachment: fixed;
+}}
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+[data-testid="stToolbar"] {{
+right: 2rem;
+}}
+</style>
+"""
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+
+# Streaming call back handler for responses
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container, initial_text=""):
+        self.container = container
+        self.text = initial_text
+
+    def on_llm_new_token(self, token: str, **kwargs):
+        self.text += token
+        self.container.markdown(self.text + "▌")
+
+# Function for Vectorizing uploaded data into Astra DB
+def vectorize_text(uploaded_file, vector_store):
+    if uploaded_file is not None:
+        
+        # Write to temporary file
+        temp_dir = tempfile.TemporaryDirectory()
+        file = uploaded_file
+        temp_filepath = os.path.join(temp_dir.name, file.name)
+        with open(temp_filepath, 'wb') as f:
+            f.write(file.getvalue())
+
+        # Load the PDF
+        docs = []
+        loader = PyPDFLoader(temp_filepath)
+        docs.extend(loader.load())
+
+        # Create the text splitter
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size = 1500,
+            chunk_overlap  = 100
+        )
+
+        # Vectorize the PDF and load it into the Astra DB Vector Store
+        pages = text_splitter.split_documents(docs)
+        vector_store.add_documents(pages)  
+        st.info(f"{len(pages)} pages loaded.")
+
+# Cache prompt for future runs
+@st.cache_data()
+def load_prompt():
+    template = """You're a helpful AI assistent tasked to answer the user's questions.
+You're friendly and you answer extensively with multiple sentences. You prefer to use bulletpoints to summarize.
+
+CONTEXT:
+{context}
+
+QUESTION:
+{question}
+
+YOUR ANSWER:"""
+    return ChatPromptTemplate.from_messages([("system", template)])
+prompt = load_prompt()
+openai_api_key = "your_api_key"
+# Cache OpenAI Chat Model for future runs
+@st.cache_resource()
+def load_chat_model():
+    return ChatOpenAI(
+        openai_api_key=openai_api_key,
+        temperature=0.3,
+        model='gpt-3.5-turbo',
+        streaming=True,
+        verbose=True
+    )
+chat_model = load_chat_model()
+
+# Cache the Astra DB Vector Store for future runs
+@st.cache_resource(show_spinner='Connecting to Astra')
+def load_vector_store():
+    # Connect to the Vector Store
+    vector_store = AstraDB(
+        embedding=OpenAIEmbeddings(openai_api_key=openai_api_key),
+        collection_name="my_store",
+        api_endpoint="your_astra_db_endpoint",
+        token="your_astra_db_token"
+    )
+    return vector_store
+vector_store = load_vector_store()
+
+# Cache the Retriever for future runs
+@st.cache_resource(show_spinner='Getting retriever')
+def load_retriever():
+    # Get the retriever for the Chat Model
+    retriever = vector_store.as_retriever(
+        search_kwargs={"k": 5}
+    )
+    return retriever
+retriever = load_retriever()
+
+# Start with empty messages, stored in session state
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+
+# Draw a title and some markdown
+st.title("Unlock Knowledge: DocuQuery - Your Intelligent Document Assistant")
+st.markdown("""DocuQuery revolutionizes how you interact with documents. 
+            Seamlessly upload your files, ask questions, and unleash the power of Retrieval Augmented Generating (RAG) technology. 
+            Gain insights, extract key information, and discover answers with unprecedented speed and accuracy. 
+            Whether it's research, study, or curiosity, DocuQuery is your gateway to a new era of document exploration.""")
+
+# Include the upload form for new data to be Vectorized
+with st.sidebar:
+    with st.form('upload'):
+        uploaded_file = st.file_uploader('Upload a document for additional context', type=['pdf'])
+        submitted = st.form_submit_button('Save to Astra DB')
+        if submitted:
+            vectorize_text(uploaded_file, vector_store)
+
+# Draw all messages, both user and bot so far (every time the app reruns)
+for message in st.session_state.messages:
+    st.chat_message(message['role']).markdown(message['content'])
+
+# Draw the chat input box
+if question := st.chat_input("What's up?"):
+    
+    # Store the user's question in a session object for redrawing next time
+    st.session_state.messages.append({"role": "human", "content": question})
+
+    # Draw the user's question
+    with st.chat_message('human'):
+        st.markdown(question)
+
+    # UI placeholder to start filling with agent response
+    with st.chat_message('assistant'):
+        response_placeholder = st.empty()
+
+    # Generate the answer by calling OpenAI's Chat Model
+    inputs = RunnableMap({
+        'context': lambda x: retriever.get_relevant_documents(x['question']),
+        'question': lambda x: x['question']
+    })
+    chain = inputs | prompt | chat_model
+    response = chain.invoke({'question': question}, config={'callbacks': [StreamHandler(response_placeholder)]})
+    answer = response.content
+
+    # Store the bot's answer in a session object for redrawing next time
+    st.session_state.messages.append({"role": "ai", "content": answer})
+
+    # Write the final answer without the cursor
+    response_placeholder.markdown(answer)
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Retrieval-Augmented-Generation-App
+# DocuQuery
+DocuQuery is an innovative document analysis and querying tool powered by Streamlit and the GPT-3.5 Turbo model. With DocuQuery, you can easily upload documents, ask questions, and receive detailed, insightful answers in seconds.
+
+## Features
+
+- RAG Technology: Leveraging Retrieval Augmented Generating (RAG) capabilities, DocuQuery offers advanced document analysis and question answering.
+- Streamlit Interface: The user-friendly Streamlit interface allows for seamless document uploads and intuitive interaction.
+- GPT-3.5 Turbo: Powered by the GPT-3.5 Turbo model, expect lightning-fast and accurate results to your queries.
+- Document Insights: Gain deep insights into your documents, extract key information, and uncover hidden patterns effortlessly.
+
+## Installation
+1. Clone the repository
+```bash
+git clone https://github.com/Mukalingam-TAS/Retrieval-Augmented-Generation-App.git
+cd Face-recognition-app-using-Streamlit
+```
+
+2. Install the dependencies
+```bash
+pip install -r requirements.txt
+```
+
+3. Run the app
+```bash
+streamlit run App.py
+```
